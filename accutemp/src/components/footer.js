b1dd28bc3e763e5187ee997eb89db85667a3ba02
Marcel Lui <marcellui123@gmail.com>
2024-06-06 16:20:33 -0400

    add icons and footer

--- accutemp/src/components/footer.js
+++ accutemp/src/components/footer.js
@@ -1,14 +1,29 @@
 import {NavLink} from "react-router-dom";
 import "./footer.css"
 import phone from "../../src/phone-call.png"
+import email from "../../src/email.png"
+import fax from "../../src/fax.png"
 
 
 function footer(){
     return(
-      <div className="container">
-        <img src={phone} className = "icon"></img>
-        <a className="phone" href = "tel:519-896-7890">(519) 896-7027</a>
-      </div>
+    <div className="background">
+         <div className="container">
+        <   img src={phone} className = "icon"></img>
+            <a className="phone" href = "tel:519-896-7890">(519) 896-7027</a>
+        </div>
+        <div className="container">
+        <   img src={email} className = "icon"></img>
+            <a className="phone" href = "mailto:info@accutempsystems.com">info@accutempsystems.com</a>
+        </div>
+        <div className="container">
+        <   img src={fax} className = "icon"></img>
+            <a className="phone" href = "tel:519-748-5510">(519) 748-5510</a>
+        </div>
+      
+      
+    </div>
+     
     )
 }
 
