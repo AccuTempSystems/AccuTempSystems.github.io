de3684eb102a952346485bd932c799ea4feb93e3
Patrick Blais <pwblais@gmail.com>
2024-06-07 10:37:50 -0400

    Fix BrowserRouter scope

--- accutemp/src/App.js
+++ accutemp/src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import NavBar from "./components/navbar"
-import Home from "./pages/Home.js"
-import Footer from "./components/footer.js"
-import {Routes, Route, BrowserRouter, Router} from "react-router-dom";
-import BuildingAutomation from "./pages/BuildingAutomation.js"
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/navbar";
+import Home from "./pages/Home.js";
+import Footer from "./components/footer.js";
+import { Routes, Route, BrowserRouter, Router } from "react-router-dom";
+import BuildingAutomation from "./pages/BuildingAutomation.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar></NavBar>
-        <Home/>
-        <Footer/>
-        <BrowserRouter>
-          <Routes>
-            <Route path="buildingautomation" element={<BuildingAutomation/>}/>
-          </Routes>
-        </BrowserRouter>
-      
-       
-        
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<BrowserRouter>
+					<NavBar></NavBar>
+					<Home />
+					<Footer />
+					<Routes>
+						<Route
+							path="buildingautomation"
+							element={<BuildingAutomation />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</header>
+		</div>
+	);
 }
 
 export default App;
