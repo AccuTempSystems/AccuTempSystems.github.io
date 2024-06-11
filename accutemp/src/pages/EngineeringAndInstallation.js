72afe8528759d2e3333b6b5a99fc8011e9e88613
Marcel Lui <marcellui123@gmail.com>
2024-06-11 12:26:57 -0400

    update engineering&installation page

--- accutemp/src/pages/EngineeringAndInstallation.js
+++ accutemp/src/pages/EngineeringAndInstallation.js
@@ -1,9 +1,15 @@
 import { NavLink } from "react-router-dom";
+import ECRA from "../ECRA_ESA_Logo_280x179.jpg"
 
 function EngineeringAndInstallation(){
     return(
-        <div>
-            <p>Hello World2</p>
+        <div className= "outside-container">
+            <img src = {ECRA}></img>
+            <h2>Whether you’re under new construction or upgrading an existing facility, our trusted engineering and installation teams can help.</h2>
+            <p>We design building automation systems to meet your priorities: comfort, energy efficiency, ease of operation and budget.</p>
+            <p>Our versatile team tackles system upgrades with the same objectives. In many cases, we can even extend the usable lifetime of your existing equipment, while improving performance and reducing energy costs.</p>
+            <p>Our installation team is made up of licensed electricians with extensive controls knowledge. They will work closely with other trades to ensure a smooth, well-executed implementation.</p>
+            <p>Accu-Temp is a licensed electrical contractor under the Electrical Safety Authority.</p>
         </div>
      
     )
--- accutemp/src/pages/ResourceManagement.js
+++ accutemp/src/pages/ResourceManagement.js
@@ -1,9 +1,16 @@
 import { NavLink } from "react-router-dom";
+import skyspark from "../skyspark-280x124.png"
 
 function ResourceManagement(){
     return(
-        <div>
-            <p>Hello World2</p>
+        <div className= "outside-container">
+           <img src={skyspark}></img>
+           <h2>Why pay for an integrated building management system if you can’t see the ROI?</h2>
+           <p>The real value is in your data. But who has time to pore over endless trend logs?</p>
+           <p> <a href = "https://www.skyfoundry.com/">SkySpark</a>, our advanced analytics platform, provides clear, real-time visualization of all your operational data. <a href = "https://www.skyfoundry.com/">SkySpark</a> alerts you, at a glance, to any equipment operating below optimum. And captures your energy consumption profile in a simple, meaningful graphical interface. Its powerful analytics engine even isolates the systems that are inefficient and predicts upcoming equipment problems, substantially reducing your operating and maintenance costs.</p>
+           <p> <a href = "https://www.skyfoundry.com/">SkySpark</a> can integrate to your business systems as well, providing accurate, consolidated information to reconcile against your operating budgets.</p>
+           <p> Our cloud-based resource management solutions will revolutionize the way you look at your building operations. </p>
+           <p> And save you money.</p>
         </div>
      
     )
